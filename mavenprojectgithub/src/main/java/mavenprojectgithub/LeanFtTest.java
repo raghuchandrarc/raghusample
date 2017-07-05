16eb7c3ed97267da35eacb56d0cf19cf81183ebd
Windows 8 <Windows 8@192.168.100.8>
2017-07-05 21:42:31 +0530

    commitsuper

---
+++ mavenprojectgithub/src/main/java/mavenprojectgithub/LeanFtTest.java
@@ -0,0 +1,28 @@
+package mavenprojectgithub;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.hp.lft.sdk.*;
+import com.hp.lft.sdk.web.*;
+import com.hp.lft.verifications.*;
+
+import unittesting.*;
+
+public class LeanFtTest extends UnitTestClassBase {
+	public static Browser driver;
+
+    
+    @Test
+    public void test() throws GeneralLeanFtException {
+    	driver=BrowserFactory.launch(BrowserType.CHROME);
+    	driver.navigate("https://www.google.co.in/");
+    	driver.describe(EditField.class, new EditFieldDescription.Builder()
+    			.id("lst-ib").build()).setValue("Facebook");;
+    
+    }
+
+}
